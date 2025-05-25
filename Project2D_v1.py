a34d462f2104369f04398f866fc81d15655b1f93
VladCiobotenco <73481862+VladCiobotenco@users.noreply.github.com>
2025-05-25 14:27:41 +0300

    Update Project2D_v2.py
    Discretizare domeniu, rezolvare sistem + vizualizare grafic

--- Project2D_v1.py
+++ Project2D_v1.py
@@ -2,18 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import lu_factor, lu_solve
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import griddata
 
-
-def discretizare_patrat(n, x, y):
-    patrate = []
-    for i in range(n):
-        for j in range(n):
-            x0, x1 = x[i], x[i + 1]
-            y0, y1 = y[j], y[j + 1]
-            patrate.append(((x0, y0), (x1, y1)))
-    return patrate
-
-def discretizez_patrate(n, x, y):
+def discretizez_patrate(x, y):
     X, Y = np.meshgrid(x, y)
     X_flat = X.flatten()
     Y_flat = Y.flatten()
@@ -39,18 +30,22 @@ def discretizare_ecuatii(puncte, x, y):
                 A[index, index + (n + 1)] = -1/h**2
     return A
 
-def plot_discretizare_patrat(squares, points):
-    fig, ax = plt.subplots()
-    for (x0, y0), (x1, y1) in squares:
-        rect = plt.Rectangle((x0, y0), x1 - x0, y1 - y0, edgecolor='black', facecolor='none')
-        ax.add_patch(rect)
-    ax.plot(points[0], points[1], 'ro', markersize=4, label="Noduri")
-    ax.set_xlim(points[0].min() - 0.1, points[0].max() + 0.1)
-    ax.set_ylim(points[1].min() - 0.1, points[1].max() + 0.1)
-    ax.set_aspect('equal')
-    plt.title("Discretizarea pătratului și nodurile")
-    plt.legend()
-    plt.grid(True)
+def vizualizare_rezultate(x_vals, y_vals, U_sys):
+    X, Y = np.meshgrid(x_vals, y_vals)
+    x_fine = np.linspace(a, b, 50)
+    y_fine = np.linspace(a, b, 50)
+    X_fine, Y_fine = np.meshgrid(x_fine, y_fine)
+    U_interp = griddata((X.flatten(), Y.flatten()), U_sys, (X_fine, Y_fine), method='cubic')
+
+
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X_fine, Y_fine, U_interp, cmap='coolwarm', edgecolor='none')
+
+    plt.title("Interpolated 3D Surface")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    ax.set_zlabel("Interpolated Values")
     plt.show()
 
 f = lambda x, y: x*2 + y*2 + 1
@@ -63,10 +58,7 @@ b = 5
 x_vals = np.linspace(a, b, n + 1)
 y_vals = np.linspace(a, b, n + 1)
 
-# Discretizare și vizualizare
-patrate = discretizare_patrat(n, x_vals, y_vals)
-puncte = discretizez_patrate(n, x_vals, y_vals)
-plot_discretizare_patrat(patrate, puncte)
+puncte = discretizez_patrate( x_vals, y_vals)
 
 A = discretizare_ecuatii(puncte, x_vals, y_vals)
 
@@ -83,30 +75,8 @@ for i in range(n + 1):
 lu, piv = lu_factor(A)
 U_sys = lu_solve((lu, piv), B)
 
-print("Matricea sistemului A:")
-print(A)
-
-print("\nVectorul termenilor liberi B:")
-print(B)
-
-print("\nSoluția sistemului U_sys:")
-print(U_sys)
+vizualizare_rezultate(x_vals, y_vals, U_sys)
 
 
-x_grid = puncte[0, :].reshape((n + 1, n + 1))
-y_grid = puncte[1, :].reshape((n + 1, n + 1))
 
-# Reshape solution to grid
-z_grid = U_sys.reshape((n + 1, n + 1))
 
-# Plotting
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(x_grid, y_grid, z_grid, cmap='coolwarm', edgecolor='none')
-fig.colorbar(surf)
-
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-plt.title("Soluția numerică pe domeniu 2D")
-
-plt.show()
